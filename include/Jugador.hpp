52d00b859f783a163dc8ac51367f965021dad850
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2011-10-10 11:09:57 -0300

    jugador

--- include/Jugador.hpp
+++ include/Jugador.hpp
@@ -7,9 +7,30 @@ class Jugador
     public:
         Jugador();
         virtual ~Jugador();
+        virtual void PrimeraRonda();
+        virtual void SegundaRonda();
+        virtual void TerceraRonda();
+        void CantarTruco();
+        void CantarReTruco();
+        void CantarValeCuatro();
+        void CantarEnvido();
+        void CantarReal();
+        void CantarEnvidoReal();
+        void CantarFalta();
+        void CantarEnvidoEnvidoFalta();
+        void CantarEnvidoFalta();
+        void CantarRealFalta();
+        void CantarEnvidoEnvido();
+
+
+        void CantarFlor();
+
+
+
 
     protected:
     private:
+
 };
 
 #endif // JUGADOR_H
