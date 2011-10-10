fa7e9380b9cced496aecc1733aa1da48ba07a876
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2011-10-10 11:32:12 -0300

    carta

--- include/Estado.hpp
+++ include/Estado.hpp
@@ -6,23 +6,23 @@ struct Estado
 {
 	Estado();
 
-	bool recibi_envido_;
-	bool recibi_real_;
-	bool recibi_envido_envido_;
-	bool recibi_envido_real_;
-	bool recibi_envido_envido_real_;
-	bool recibi_envido_falta_;
-	bool recibi_envido_envido_falta_;
-	bool recibi_envido_real_falta_;
-	bool recibi_real_falta_;
+	bool envido_;
+	bool real_;
+	bool envido_envido_;
+	bool envido_real_;
+	bool envido_envido_real_;
+	bool envido_falta_;
+	bool envido_envido_falta_;
+	bool envido_real_falta_;
+	bool real_falta_;
 	bool envido_envido_real_falta_;
-	bool recibi_falta_;
+	bool falta_;
 
-	bool recibi_flor_;
+	bool flor_;
 
-	bool recibi_truco_;
-	bool recibi_retruco_;
-	bool recibi_vale_cuatro_;
+	bool truco_;
+	bool retruco_;
+	bool vale_cuatro_;
 };
 
 #endif // ESTADO_H
