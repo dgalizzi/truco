b782191ee9f04eb75f753df6a78e35f91b8c8d49
Diego D. Galizzi <dgalizzi@gmail.com>
2011-10-10 11:39:30 -0300

    Estado false

--- src/Estado.cpp
+++ src/Estado.cpp
@@ -2,19 +2,19 @@
 
 Estado::Estado()
 {
-	envido_ = false;
-	real_ = false;
-	envido_envido_ = false;
-	envido_real_ = false;
-	envido_envido_real_ = false;
-	envido_falta_ = false;
-	envido_envido_falta_ = false;
-	envido_real_falta_ = false;
-	real_falta_ = false;
+	envido_                   = false;
+	real_                     = false;
+	envido_envido_            = false;
+	envido_real_              = false;
+	envido_envido_real_       = false;
+	envido_falta_             = false;
+	envido_envido_falta_      = false;
+	envido_real_falta_        = false;
+	real_falta_               = false;
 	envido_envido_real_falta_ = false;
-	falta_ = false;
-	flor_ = false;
-	truco_ = false;
-	retruco_ = false;
-	vale_cuatro_ = false;
+	falta_                    = false;
+	flor_                     = false;
+	truco_                    = false;
+	retruco_                  = false;
+	vale_cuatro_              = false;
 }
