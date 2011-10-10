5d9af9684d35a636be02310556f0cd3e2c1b0064
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2011-10-10 11:28:36 -0300

    queeen

--- include/Estado.hpp
+++ include/Estado.hpp
@@ -8,33 +8,21 @@ struct Estado
 
 	bool recibi_envido_;
 	bool recibi_real_;
-	bool recibi_real_falta_;
-
-	bool recibi_envido_real_;
-	bool recibi_envido_real_falta_;
-	bool recibi_envido_falta_;
 	bool recibi_envido_envido_;
+	bool recibi_envido_real_;
 	bool recibi_envido_envido_real_;
+	bool recibi_envido_falta_;
 	bool recibi_envido_envido_falta_;
-	bool recibi_envido_envido_real_falta_;
-
+	bool recibi_envido_real_falta_;
+	bool recibi_real_falta_;
+	bool envido_envido_real_falta_;
 	bool recibi_falta_;
-
-
-
-
-
-
-
 
 	bool recibi_flor_;
 
 	bool recibi_truco_;
 	bool recibi_retruco_;
 	bool recibi_vale_cuatro_;
-
-
-
 };
 
 #endif // ESTADO_H
